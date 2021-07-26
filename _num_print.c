fdcc2ca85e7a21a17154319c52591bfed1acf2e7
Andres Ortiz <3283@holbertonschool.com>
2021-07-26 10:51:13 -0700

    Added num_print

--- _num_print.c
+++ _num_print.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * _num_print - prints string
+ * _num_print - prints digits
  * @n: integer to be printed
  *
  * Return: n
@@ -12,18 +12,18 @@ int _num_print(int n)
 	unsigned int num = n;
 	int counter;
 
-	if(n)
-	{
 	if (n < 0)
 	{
 		_putchar('-');
 		num *= -1;
 	}
-	if ((num / 10) > 0)
+	if (num / 10)
 	{
 		_num_print(num / 10);
 	}
-	counter = _putchar((num % 10) + '0');
-	}
-	return (counter);
+	if (num == 0)
+		_putchar('0');
+	else
+		counter = _putchar((num % 10) + '0');
+	return(counter);
 }
