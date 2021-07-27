812e26f48aa6b749ca0ef47fbe9373c1f7f44e29
Andres Ortiz <3283@holbertonschool.com>
2021-07-27 11:43:23 -0700

    added int to bin

--- _int_to_bin.c
+++ _int_to_bin.c
@@ -1,17 +1,11 @@
 #include "holberton.h"
-/**
- * nbr_size - counts size returned
- * @n: int to be printed
- *
- * Return: size printed
- */
+
 /**
  * _int_to_bin - print integer to binary
  * @n: integer to be printed
  *
  * Return: size
  */
-
 int _int_to_bin(unsigned int n)
 {
 	int count = 0;
@@ -29,5 +23,5 @@ int _int_to_bin(unsigned int n)
 	if ((n / 2) != 0)
 		_int_to_bin(n / 2);
 	else
-	return(count);
+	return (count);
 }
