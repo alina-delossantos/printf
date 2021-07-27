04d8d107b7d99cb810223aa1cd9bed67f97c19b2
alina-delossantos <alina.delossantos@gmail.com>
2021-07-27 11:31:05 -0700

    new printf

--- _printf.c
+++ _printf.c
@@ -1,9 +1,12 @@
 #include "holberton.h"
+
 /**
  * _printf - produces output according to a format
  * @format: character string
+ *
  * Return: number of characters printed
  */
+
 int _printf(const char *format, ...)
 {
 	va_list list;
@@ -30,7 +33,7 @@ int _printf(const char *format, ...)
 				ck = 1;
 		}
 		else
-			{
+			     {
 			switch (format[i])
 			{
 			case 'c':
